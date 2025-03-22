3fd668ba73a2a0d39f3e1bc54326c0081cc64d2e
Radzivon Bartoshyk <radzivon.bartoshyk@proton.me>
2025-03-22 21:39:29 +0000

    BPC improvements

--- src/conversions/bpc.rs
+++ src/conversions/bpc.rs
@@ -29,7 +29,8 @@
 
 use crate::conversions::interpolator::{MultidimensionalInterpolation, Tetrahedral};
 use crate::conversions::transform_lut4_to_4::{NonFiniteVector3fLerp, Vector3fCmykLerp};
-use crate::{ColorProfile, DataColorSpace, Lab, Xyz};
+use crate::mlaf::mlaf;
+use crate::{Chromaticity, ColorProfile, DataColorSpace, Lab, Xyz};
 
 impl ColorProfile {
     #[inline]
@@ -61,24 +62,24 @@ impl ColorProfile {
             let m = 65535;
             let y = 65535;
             let k = 65535;
-
+            
             let linear_k: f32 = k as f32 * (1. / 65535.);
             let w: i32 = k * (GRID_SIZE as i32 - 1) / 65535;
             let w_n: i32 = (w + 1).min(GRID_SIZE as i32 - 1);
             let t: f32 = linear_k * (GRID_SIZE as i32 - 1) as f32 - w as f32;
-
+            
             let grid_size = GRID_SIZE as i32;
             let grid_size3 = grid_size * grid_size * grid_size;
-
+            
             let table1 = &lut[(w * grid_size3 * 3) as usize..];
             let table2 = &lut[(w_n * grid_size3 * 3) as usize..];
-
+            
             let tetrahedral1 = Tetrahedral::<GRID_SIZE>::new(table1);
             let tetrahedral2 = Tetrahedral::<GRID_SIZE>::new(table2);
             let r1 = tetrahedral1.inter3(c, m, y);
             let r2 = tetrahedral2.inter3(c, m, y);
             let r = NonFiniteVector3fLerp::interpolate(r1, r2, t, 1.0);
-
+            
             let mut lab = Lab::from_xyz(Xyz {
                 x: r.v[0],
                 y: r.v[1],
@@ -90,7 +91,7 @@ impl ColorProfile {
                 lab.l = 50.;
             }
             let bp = lab.to_xyz();
-
+            
             return Some(bp);
         }
         if self.color_space == DataColorSpace::Rgb {
@@ -101,16 +102,21 @@ impl ColorProfile {
 }
 
 pub(crate) fn compensate_bpc_in_lut(lut_xyz: &mut [f32], src_bp: Xyz, dst_bp: Xyz) {
-    // let rx = (1. - dst_bp.x) / (1. - src_bp.x);
-    // let ry = (1. - dst_bp.y) / (1. - src_bp.y);
-    // let rz = (1. - dst_bp.z) / (1. - src_bp.z);
-    // let dx = 1. - rx;
-    // let dy = 1. - ry;
-    // let dz = 1. - rz;
-    // const WP_50: Xyz = Chromaticity::D50.to_xyz();
-    // for dst in lut_xyz.chunks_exact_mut(3) {
-    //     dst[0] = mlaf(dst[0] * rx, dx, WP_50.x);
-    //     dst[1] = mlaf(dst[1] * ry, dy, WP_50.y);
-    //     dst[2] = mlaf(dst[2] * rz, dz, WP_50.z);
-    // }
+    const WP_50: Xyz = Chromaticity::D50.to_xyz();
+    let tx = src_bp.x - WP_50.x;
+    let ty = src_bp.y - WP_50.y;
+    let tz = src_bp.z - WP_50.z;
+    let ax = (dst_bp.x - WP_50.x) / tx;
+    let ay = (dst_bp.y - WP_50.y) / ty;
+    let az = (dst_bp.z - WP_50.z) / tz;
+
+    let bx = -WP_50.x * (dst_bp.x - src_bp.x) / tx;
+    let by = -WP_50.y * (dst_bp.y - src_bp.y) / ty;
+    let bz = -WP_50.z * (dst_bp.z - src_bp.z) / tz;
+
+    for dst in lut_xyz.chunks_exact_mut(3) {
+        dst[0] = mlaf(bx, dst[0], ax);
+        dst[1] = mlaf(by, dst[1], ay);
+        dst[2] = mlaf(bz, dst[2], az);
+    }
 }
