6d8c08fd2688365476bb23e404ac0a9e2347d554
Radzivon Bartoshyk <radzivon.bartoshyk@proton.me>
2026-04-17 08:08:52 +0100

    Making dispatch slightly better

--- src/conversions/avx/mod.rs
+++ src/conversions/avx/mod.rs
@@ -36,7 +36,7 @@ mod rgb_xyz_q2_13_opt;
 mod t_lut3_to_3;
 mod t_lut3_to_3_q0_15;
 
-#[cfg(feature = "lut")]
+#[cfg(feature = "avx_luts")]
 use crate::conversions::interpolator::BarycentricWeight;
 #[cfg(feature = "avx_luts")]
 pub(crate) use lut4_to_3::AvxLut4x3Factory;
@@ -50,7 +50,7 @@ pub(crate) use rgb_xyz_q2_13_opt::TransformShaperRgbQ2_13OptAvx;
 pub(crate) use t_lut3_to_3::AvxLut3x3Factory;
 
 // this is required to ensure that interpolator never goes out of bounds
-#[cfg(feature = "lut")]
+#[cfg(feature = "avx_luts")]
 fn assert_barycentric_lut_size_precondition<R, const GRID_SIZE: usize>(
     lut: &[BarycentricWeight<R>],
 ) {
